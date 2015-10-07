4ef6ab2968ac78316fded89b6d7ad1add6dee2ed
Konrad Kowalewski <k.j.kowalewski@gmail.com>
2015-10-07 09:49:25 +0200

    New webpage screens added: session details, signup.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var pdf = require('../dantooine_modules/document-generator/documentGenerator');
 
+/* PRIVATE ZONE */
+
 /* GET dashboard */
 router.get('/dashboard', function (req, res, next) {
   res.render('dashboard', { title: 'Dashboard' });
@@ -22,12 +24,25 @@ router.get('/sessions', function (req, res, next) {
     res.render('sessions', { title: 'Sesje i głosowania' });
 });
 
-/* GET session info */
-router.get('/session', function (req, res, next) {
-    res.render('session', { title: 'Sesja M-15-8-REG' });
+router.post('/sessions', function (request, response, next) {
+    console.log('Podano ' + request.body.name);
+    var id = 1;
+    response.redirect('/session/'+id);
 });
 
-/* GET council overview */
+/* POST new session voting*/
+router.post('voting', function (request, response, next) {
+    var id = request.body.session_id;
+    response.redirect('/session/'+id);
+});
+
+/* GET session info */
+router.get('/session/:id', function (request, response, next) {
+    var id = request.params.id;
+    response.render('session', { title: 'Sesja M-15-8-REG', session_id: id });
+});
+
+/* GET voters overview */
 router.get('/voters', function (req, res, next) {
     res.render('voters', { title: 'Rada Wydziału' });
 });
@@ -36,7 +51,14 @@ router.get('/voters', function (req, res, next) {
 router.post('/voters', function (request, res, next) {
     console.log('Podano ' + request.body.last_name);
     res.redirect('/voters');
-})
+});
+
+/* PUBLIC ZONE */
+
+/* GET signup page */
+router.get('/signup', function (req, res, next) {
+    res.render('signup', { title: 'Elektroniczne głosowanie', system: 'System elektronicznego głosowania Rady Wydziału' });
+});
 
 /* GET home page */
 router.get('/', function(req, res, next) {
